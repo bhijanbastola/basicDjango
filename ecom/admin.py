380185f70b16893c9413fd727c747333eb9e51ef
bhijanbastola <bhjanbastola@gmail.com>
2025-07-24 22:11:31 +0545

    admin panel customization

--- ecom/admin.py
+++ ecom/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductReview, ProductCertificate, Store
 
-# Register your models here.
-admin.site.register(Product)
+class ProductReviewInline(admin.TabularInline):
+    model = ProductReview
+    extra = 2
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'price', 'date_added')
+    inlines = [ProductReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('store', )  
+
+class ProductCertificateAdmin(admin.ModelAdmin):
+    list_display = ('product',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ProductCertificate, ProductCertificateAdmin)
--- ecom/models.py
+++ ecom/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -18,6 +19,49 @@ class Product(models.Model):
     date_added=models.DateTimeField(default=timezone.now)
     type=models.CharField(max_length=20,choices=PRODUCT_TYPE_CHOICES)
     description=models.TextField(default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    stock=models.PositiveIntegerField(default=0)
+
+
 
     def __str__(self):
         return self.name
+
+#One to many Relationship
+class ProductReview(models.Model):
+    REVIEWS=[
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5'),
+
+    
+    ]    
+    product =models.ForeignKey(Product, on_delete=models.CASCADE,related_name='reviews')
+    User=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.TextField(default='')
+    date=models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return f'{self.User.username} review for {self.product.name}'  
+
+
+#Many to many relationship
+class Store(models.Model):
+    name=models.CharField( max_length=50)
+    location=models.CharField( max_length=50)
+    store=models.ManyToManyField(Product,related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+
+#One to one
+class ProductCertificate(models.Model):
+    product=models.OneToOneField(Product,on_delete=models.CASCADE,related_name='certificate')
+
+
+    def __str__(self):
+        return f'Certificate for {self.name.product}'
